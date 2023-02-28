3bf001eddf0aa13699e500793649df45572e17aa
Isabela Silva Bellizzi <112705453+isabelasbellizzi@users.noreply.github.com>
2023-02-28 14:01:42 -0300

    Delete infra directory

--- infra/orm/mappings/__init__.py
+++
@@ -1,7 +0,0 @@
-from .function_mapping import function_mapping
-from .paper_mapping import paper_mapping
-from .system_mapping import system_mapping
-from .user_mapping import user_mapping
-from .feature_mapping import feature_mapping
-from .user_permission_mapping import user_permission_mapping
-from .owner_mapping import owner_mapping
--- infra/repositories/interfaces/isystem_permission_repository.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class ISystemPermissionRepository(ABC):
-    @abstractmethod
-    def get_all(self, system_id: int):  # -> o que colocar como retorno?
-        raise Exception("Not implemented")
